a34f0e90555b1a4a18cf68ad45b9317f6884c6b9
Hari <harinath.b.07@gmail.com>
2022-04-09 14:12:21 +0530

    fibonacci program completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let fibo =[1, 1];
+    for (let i = 2; i < n; i++) {
+        fibo[i] = fibo[i - 1] + fibo [i - 2];
+    }
+     return fibo[n - 1];
 };
 
 // Do not edit below this line
