c88a94314d0d4b2b380e36ba2e6626c78b7bcc1a
Hari <harinath.b.07@gmail.com>
2022-04-09 14:37:00 +0530

    Oldest person found

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    ages = people.map(person => 
+        getAge(person.yearOfBirth, person.yearOfDeath)
+    );
+    return people[ages.indexOf(Math.max(...ages))];
 };
+
+function getAge(birth, death) {
+    if (death == undefined) {
+        return new Date().getFullYear() - birth;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
