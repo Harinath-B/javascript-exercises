d460ad12a5e6298dcfc057999bce61ee56f6d298
Hari <harinath.b.07@gmail.com>
2022-04-09 14:07:36 +0530

    palindrome program completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-z]/g, '');
+    
+    return word.split('').reverse().join('') === word;
 };
 
 // Do not edit below this line
